0e0115b047ec6a5a8ba6fb2b1c533e3927ac539f
marinamedeirosz <marinapaffer@gmail.com>
2025-03-15 12:19:49 -0300

    feat: add Formatter and Validator classes for CPF and phone number handling

---
+++ src/main/java/com/marina/util/Formatter.java
@@ -0,0 +1,37 @@
+package com.marina.util;
+
+public class Formatter {
+    public static String formatCpf(String cpf) {
+        if (cpf == null || cpf.isEmpty()) {
+            return "";
+        }
+        if (cpf.length() != 11) {
+            return cpf;
+        }
+        return cpf.replaceAll("(\\d{3})(\\d{3})(\\d{3})(\\d{2})", "$1.$2.$3-$4");
+    }
+
+    public static String unformatCpf(String cpf) {
+        if (cpf == null || cpf.isEmpty()) {
+            return "";
+        }
+        if (cpf.length() != 14) {
+            return cpf;
+        }
+        return cpf.replaceAll("\\.", "").replaceAll("-", "");
+    }
+
+    public static String formatPhone(String phone) {
+        if (phone == null || phone.isEmpty()) {
+            return "";
+        }
+        return phone.replaceAll("(\\d{2})(\\d{5})(\\d{4})", "($1) $2-$3");
+    }
+    
+    public static String unformatPhone(String phone) {
+        if (phone == null || phone.isEmpty()) {
+            return "";
+        }
+        return phone.replaceAll("\\(", "").replaceAll("\\)", "").replaceAll("-", "");
+    }
+}
---
+++ src/main/java/com/marina/util/Validator.java
@@ -0,0 +1,42 @@
+package com.marina.util;
+
+import com.marina.exception.InvalidCpfException;
+
+public class Validator {
+    public static boolean isValid(String cpf) {
+        cpf = cpf.replaceAll("[^0-9]", "");
+
+        if (cpf.length() != 11) {
+            throw new InvalidCpfException("deve ter 11 dígitos.");
+        }
+
+        if (cpf.matches("(\\d)\\1{10}")) {
+            throw new InvalidCpfException("não pode ter todos os dígitos iguais.");
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            sum += (cpf.charAt(i) - '0') * (10 - i);
+        }
+        int firstDigit = 11 - (sum % 11);
+        if (firstDigit == 10 || firstDigit == 11) {
+            firstDigit = 0;
+        }
+
+        sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += (cpf.charAt(i) - '0') * (11 - i);
+        }
+        int secondDigit = 11 - (sum % 11);
+        if (secondDigit == 10 || secondDigit == 11) {
+            secondDigit = 0;
+        }
+
+        return cpf.charAt(9) == (char) ('0' + firstDigit) && cpf.charAt(10) == (char) ('0' + secondDigit);
+    }
+
+    public static boolean isValidPhone(String phone) {
+        phone = phone.replaceAll("[^0-9]", "");
+        return phone.length() == 11;
+    }
+}

d4029d4914f9c9ca4258aa230bd4adfe5f93ecd6
marinamedeirosz <marinapaffer@gmail.com>
2025-03-15 16:22:04 -0300

    feat: add Style utility class for printing formatted lines and heart shapes

---
+++ src/main/java/com/marina/utils/Style.java
@@ -0,0 +1,27 @@
+package com.marina.utils;
+
+public class Style {
+    public static void printLine(int length) {
+        System.out.println("-" + " ".repeat(length) + "-");
+    }
+    
+    public static void printHeart() {
+        String[] linhas = {
+            "",
+            "      *****     *****",
+            "    **     ** **     **",
+            "   **       ***       **",
+            "   **                 **",
+            "    **               **",
+            "      **           **",
+            "        **       **",
+            "          **   **",
+            "            **",
+            ""
+        };
+        
+        for (String linha : linhas) {
+            System.out.println(linha);
+        }
+    }
+}

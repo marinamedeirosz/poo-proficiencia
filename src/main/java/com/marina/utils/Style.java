6253204e7e2129cdf8e8ba0f781330c2342571c6
marinamedeirosz <marinapaffer@gmail.com>
2025-03-16 13:49:58 -0300

    refactor: simplify line printing in Style class by removing unnecessary whitespace, enhancing output clarity

--- src/main/java/com/marina/utils/Style.java
+++ src/main/java/com/marina/utils/Style.java
@@ -2,7 +2,7 @@ package com.marina.utils;
 
 public class Style {
     public static void printLine(int length) {
-        System.out.println("-" + " ".repeat(length) + "-");
+        System.out.println("-".repeat(length));
     }
     
     public static void printHeart() {
